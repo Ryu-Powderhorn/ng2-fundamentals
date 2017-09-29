efb3c8c995675115374ad4f942c7c1d5f44cea40
Tristan JC Rouse Powderhorn <ryu.powderhorn@cottageindustries.build>
2017-09-29 03:38:27 -0500

    wip

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -1,22 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from "@angular/forms";
+import { FormControl, Validators, FormGroup } from "@angular/forms";
+import { ISession } from '../index';
 
 @Component({
     templateUrl: '/app/events/event-details/create-session.component.html',
 })
 export class CreateSessionComponent implements OnInit {
 
-    name
-    duration
-    level
-    abstract
+    newSessionForm: FormGroup
+
+    name: FormControl
+    duration: FormControl
+    presenter: FormControl 
+    level: FormControl
+    abstract: FormControl
     
     constructor() { }
-
+    
     ngOnInit() {
         this.name = new FormControl('', Validators.required)
         this.duration = new FormControl('', Validators.required)
         this.level = new FormControl('', Validators.required)
         this.abstract = new FormControl('', Validators.required)
-        this.name = new FormControl('', Validators.required, Validators.maxLength(400)) }
+        this.name = new FormControl('',   ) 
+        
+        this.newSessionForm = new FormGroup({
+            name: this.name,
+            presenter: this.presenter,
+            duration: this.duration,
+            abstract: this.abstract
+        })
+
+    }
+
+    saveSession(formValues) {
+        let session: ISession = {
+            id:undefined,
+            name:formValues.name,
+            duration:formValues.duration,
+            level:formValues.level,
+            presenter:formValues.presenter,
+            abstract:formValues.abstract,
+            voters: []
+        }
+    }
 }
